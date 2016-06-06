f98d4818f8639dd7f7fcd15153f0452bbeaa75d3
Menakanagaraj <menakanagaraj18@gmail.com>
2016-06-06 20:29:16 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int i,n,count=0;
+   scanf("%d",&n);
+   for(i=2;i<n/2;i++)
+   {
+       if(n%i==0)
+       count++;
+   }
+   if(count==0)
+   printf("Prime number");
+   else
+   printf("Not a prime number");
+    return 0;
+}
